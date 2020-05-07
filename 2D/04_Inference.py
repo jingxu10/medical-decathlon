6f5a16157c31ada16e88094d32f4ae1c3a32b999
yupingzh <yuping.zhao@intel.com>
2020-05-07 21:32:54 +0800

    updated 04_Inference.py and inference.sh

--- 2D/04_Inference.py
+++ 2D/04_Inference.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 import keras as K
 import h5py
+import math
 import time
 
 from data import load_data
@@ -13,14 +14,26 @@ from model import unet
 
 from argparser import args
 
+intra=0
+inter=0
+if 'intra_op_parallelism_threads' in os.environ:
+    intra = int(os.environ['intra_op_parallelism_threads'])
+if 'inter_op_parallelism_threads' in os.environ:
+    inter = int(os.environ['inter_op_parallelism_threads'])
+if intra != 0 and inter != 0:
+    config = tf.ConfigProto(intra_op_parallelism_threads=intra,
+            inter_op_parallelism_threads=inter)
+    session = tf.Session(config=config)
+    K.backend.set_session(session)
+
 print ("We are using Tensorflow version", tf.__version__, "with Intel(R) oneDNN", "enabled" if tf.pywrap_tensorflow.IsMklEnabled() else "disabled",)
 
 data_path = os.path.join("../../data/decathlon/144x144/")
 data_filename = "Task01_BrainTumour.h5"
 hdf5_filename = os.path.join(data_path, data_filename)
 imgs_train, msks_train, imgs_validation, msks_validation, imgs_testing, msks_testing = load_data(hdf5_filename)
-imgs_warmup=imgs_testing[:500]
-imgs_infere=imgs_testing[500:2500]
+imgs_warmup=imgs_testing[:10]
+imgs_infere=imgs_testing[10:266]
 print("Number of imgs_warmup: {}".format(imgs_warmup.shape[0]))
 print("Number of imgs_infere: {}".format(imgs_infere.shape[0]))
 
@@ -44,7 +57,8 @@ def do_benchmark(batch_size=32):
     t0 = time.time()
     model.predict(imgs_infere, batch_size, verbose=1, steps=None)
     t1 = time.time()
-    lat=t1-t0
+    num = math.ceil(imgs_infere.shape[0]/batch_size)
+    lat=(t1-t0)/num
     thr=batch_size/lat
     print('Latency: {:.2f}ms; Throughput: {:.2f}fps'.format(lat*1000, thr))
 
--- 2D/inference.sh
+++ 2D/inference.sh
@@ -9,13 +9,13 @@
 
 if [[ $# -ge 1 ]] && [[ $1 == 'opt' ]]; then
     echo "Set environment variables"
-    export OMP_NUM_THREADS=28
+    export OMP_NUM_THREADS=26
     export KMP_BLOCKTIME=1
     export KMP_AFFINITY=granularity=fine,compact
     export KMP_SETTINGS=0
-    export intra_op_parallelism_threads=28
+    export intra_op_parallelism_threads=26
     export inter_op_parallelism_threads=1
 fi
 
-source /mnt/sdb/jingxu1/framework/tensorflow/venvs/venv_intel_pip_py37/bin/activate
+source /root/tf_unet2d/venv_tf_unet2d_py37/bin/activate
 python 04_Inference.py --batch_size 1
